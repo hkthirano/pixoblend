8962b3e1bc716ab309cf1e3113e4fac98e9b6003
hokuto.hiarno2 <hokuto.hiarno2@evidentscientific.com>
2023-06-04 19:08:32 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import OpenSeaDragon from "openseadragon";
+// import { useEffect, useState } from "react";
+// import OpenSeaDragon from "openseadragon";
 
 export default function App() {
-  const [viewer, setViewer] = useState(null);
+  // const [viewer, setViewer] = useState(null);
 
-  const InitOpenseadragon = () => {
-    viewer && viewer.destroy();
+  // const InitOpenseadragon = () => {
+  //   viewer && viewer.destroy();
 
-    setViewer(
-      OpenSeaDragon({
-        id: "openSeaDragon",
-        animationTime: 0.5,
-        blendTime: 0.1,
-        constrainDuringPan: true,
-        maxZoomPixelRatio: 2,
-        minZoomLevel: 1,
-        visibilityRatio: 1,
-        zoomPerScroll: 2,
-        tileSources: {
-          type: "legacy-image-pyramid",
-          levels: [
-            {
-              url: require("./images/mapExmpl.jpg"),
-              height: 2000,
-              width: 2000
-            },
-            {
-              url: "http://sergiopedercini.com/test-images/test-image-02.png",
-              height: 4000,
-              width: 4000
-            },
-            {
-              url: "http://sergiopedercini.com/test-images/test-image-03.png",
-              height: 6000,
-              width: 6000
-            }
-          ]
-        }
-      })
-    );
-  };
-  useEffect(() => {
-    InitOpenseadragon();
-  }, []);
+  //   setViewer(
+  //     OpenSeaDragon({
+  //       id: "openSeaDragon",
+  //       animationTime: 0.5,
+  //       blendTime: 0.1,
+  //       constrainDuringPan: true,
+  //       maxZoomPixelRatio: 2,
+  //       minZoomLevel: 1,
+  //       visibilityRatio: 1,
+  //       zoomPerScroll: 2,
+  //       tileSources: {
+  //         type: "legacy-image-pyramid",
+  //         levels: [
+  //           {
+  //             url: require("./images/mapExmpl.jpg"),
+  //             height: 2000,
+  //             width: 2000
+  //           },
+  //           {
+  //             url: "http://sergiopedercini.com/test-images/test-image-02.png",
+  //             height: 4000,
+  //             width: 4000
+  //           },
+  //           {
+  //             url: "http://sergiopedercini.com/test-images/test-image-03.png",
+  //             height: 6000,
+  //             width: 6000
+  //           }
+  //         ]
+  //       }
+  //     })
+  //   );
+  // };
+  // useEffect(() => {
+  //   InitOpenseadragon();
+  // }, []);
 
   return (
     <div
@@ -53,6 +53,6 @@ export default function App() {
         width: "800px",
         height: "800px"
       }}
-    ></div>
+    >test</div>
   );
 }
