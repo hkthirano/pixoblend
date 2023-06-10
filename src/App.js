f6848e77ceaa23e1e691def85d763c4c10f1e0bc
hokuto.hiarno2 <hokuto.hiarno2@evidentscientific.com>
2023-06-10 13:05:52 +0900

    シンプルイメージ表示

--- src/App.js
+++ src/App.js
@@ -15,28 +15,12 @@ export default function App() {
         blendTime: 0.1,
         constrainDuringPan: true,
         maxZoomPixelRatio: 2,
-        minZoomLevel: 1,
+        minZoomLevel: 0.5,
         visibilityRatio: 1,
         zoomPerScroll: 2,
         tileSources: {
-          type: "legacy-image-pyramid",
-          levels: [
-            {
-              url: require("./images/mapExmpl.jpg"),
-              height: 2000,
-              width: 2000
-            },
-            {
-              url: "http://sergiopedercini.com/test-images/test-image-02.png",
-              height: 4000,
-              width: 4000
-            },
-            {
-              url: "http://sergiopedercini.com/test-images/test-image-03.png",
-              height: 6000,
-              width: 6000
-            }
-          ]
+          type: "image",
+          url: require("./images/mapExmpl.jpg"),
         }
       })
     );
